52660a06a27c37b4dc678784c8d4cb2543cfc735
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-09-14 12:12:35 -0400

    slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
+$(function () {
+  $(".slider__items").slick({
+    prevArrow:
+      '<img class="slider__arrow slider__arrow-left" src="/images/arrow-left.svg" alt="left" />',
+    nextArrow:
+      '<img class="slider__arrow slider__arrow-right" src="/images/arrow-right.svg" alt="left" />',
+    fade: true,
+  });
 });
